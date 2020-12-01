ca9389c2da9754418d28c636b6cc69db972538b4
safwanvk <safwanvalakundil@gmail.com>
2020-12-01 18:04:38 +0530

    :zap: Change view for company list api

--- core/views.py
+++ core/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 # ViewSets define the view behavior.
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Company
 from .serializers import CompanySerializer
 
 
-class CompanyViewSet(viewsets.ModelViewSet):
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
+# class CompanyViewSet(viewsets.ModelViewSet):
+#     queryset = Company.objects.all()
+#     serializer_class = CompanySerializer
+
+class CompanyViewSet(viewsets.ViewSet):
+    def list(self, request):
+        company = Company.objects.all()
+        serializer = CompanySerializer(company, many=True, context={"request": request})
+        response_dict = {"error": False, "message": "All Company List Data", "data": serializer.data}
+        return Response(response_dict)
+
+
+company_list = CompanyViewSet.as_view({"get":"list"})
