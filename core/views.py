bbe5d155a18317da1017b765d88543c25aa07a6e
safwanvk <safwanvalakundil@gmail.com>
2020-12-01 18:16:45 +0530

    :zap: Setup view for company data update api

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 # ViewSets define the view behavior.
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from .models import Company
@@ -30,6 +31,20 @@ class CompanyViewSet(viewsets.ViewSet):
             dict_response = {"error": True, "message": "Error During Saving Company Data"}
         return Response(dict_response)
 
+    def update(self, request, pk=None):
+        try:
+            queryset = Company.objects.all()
+            company = get_object_or_404(queryset, pk=pk)
+            serializer = CompanySerializer(company, data=request.data, context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            dict_response = {"error": False, "message": "Successfully Updated Company Data"}
+        except:
+            dict_response = {"error": True, "message": "Error During Updating Company Data"}
+
+        return Response(dict_response)
+
 
 company_list = CompanyViewSet.as_view({"get":"list"})
 company_create = CompanyViewSet.as_view({"post":"create"})
+company_update = CompanyViewSet.as_view({"put":"update"})
