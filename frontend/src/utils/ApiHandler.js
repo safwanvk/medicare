3429a58a63a053c32fe583a4cf931ae0de0c149e
safwanvk <safwanvalakundil@gmail.com>
2020-12-11 15:41:37 +0530

    :art: Setup add company bank

--- frontend/src/utils/ApiHandler.js
+++ frontend/src/utils/ApiHandler.js
@@ -99,6 +99,23 @@ async fetchAllCompany() {
     return response;
   }
 
+  async saveCompanyBankData(bank_account_no, ifsc_no, company_id) {
+    await this.checkLogin();
+    //Wait Until Token Get Updated
+
+    var response = await Axios.post(
+      Config.companyBankApiUrl,
+      {
+        bank_account_no: bank_account_no,
+        ifsc_no: ifsc_no,
+        company_id: company_id,
+      },
+      { headers: { Authorization: "Bearer " + AuthHandler.getLoginToken() } }
+    );
+
+    return response;
+  }
+
 }
 
 
