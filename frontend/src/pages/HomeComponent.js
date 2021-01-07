3d7b5c1089997c369391ec6e0c32a68618760c13
safwanvk <safwanvalakundil@gmail.com>
2021-01-07 15:46:45 +0530

    :art: Setup dashboard layout

--- frontend/src/pages/HomeComponent.js
+++ frontend/src/pages/HomeComponent.js
@@ -1,6 +1,25 @@
 import React from "react";
 
 class HomeComponent extends React.Component {
+
+  state = {
+    customer_request: 0,
+    bill_count: 0,
+    medicine_count: 0,
+    company_count: 0,
+    employee_count: 0,
+    profit_total: 0,
+    sell_total: 0,
+    request_pending: 0,
+    request_completed: 0,
+    profit_amt_today: 0,
+    sell_amt_today: 0,
+    medicine_expire_serializer_data: 0,
+    dataPoints: [],
+    profitChartOption: {},
+    sellChartOption: {},
+  };
+
   render() {
     return (
       <section className="content">
@@ -13,10 +32,10 @@ class HomeComponent extends React.Component {
             <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="info-box bg-pink hover-expand-effect">
                 <div className="icon">
-                  <i className="material-icons">playlist_add_check</i>
+                  <i className="material-icons">bookmark</i>
                 </div>
                 <div className="content">
-                  <div className="text">NEW TASKS</div>
+                  <div className="text">TOTAL REQUEST</div>
                   <div
                     className="number count-to"
                     data-from="0"
@@ -24,7 +43,7 @@ class HomeComponent extends React.Component {
                     data-speed="15"
                     data-fresh-interval="20"
                   >
-                    125
+                    {this.state.customer_request}
                   </div>
                 </div>
               </div>
@@ -32,10 +51,10 @@ class HomeComponent extends React.Component {
             <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="info-box bg-cyan hover-expand-effect">
                 <div className="icon">
-                  <i className="material-icons">help</i>
+                  <i className="material-icons">bookmark</i>
                 </div>
                 <div className="content">
-                  <div className="text">NEW TICKETS</div>
+                  <div className="text">TOTAL SALES</div>
                   <div
                     className="number count-to"
                     data-from="0"
@@ -43,7 +62,7 @@ class HomeComponent extends React.Component {
                     data-speed="1000"
                     data-fresh-interval="20"
                   >
-                    257
+                    {this.state.bill_count}
                   </div>
                 </div>
               </div>
@@ -51,10 +70,10 @@ class HomeComponent extends React.Component {
             <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="info-box bg-light-green hover-expand-effect">
                 <div className="icon">
-                  <i className="material-icons">forum</i>
+                  <i className="material-icons">bookmark</i>
                 </div>
                 <div className="content">
-                  <div className="text">NEW COMMENTS</div>
+                  <div className="text">TOTAL MEDICINES</div>
                   <div
                     className="number count-to"
                     data-from="0"
@@ -62,7 +81,7 @@ class HomeComponent extends React.Component {
                     data-speed="1000"
                     data-fresh-interval="20"
                   >
-                    243
+                    {this.state.medicine_count}
                   </div>
                 </div>
               </div>
@@ -70,10 +89,10 @@ class HomeComponent extends React.Component {
             <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="info-box bg-orange hover-expand-effect">
                 <div className="icon">
-                  <i className="material-icons">person_add</i>
+                  <i className="material-icons">bookmark</i>
                 </div>
                 <div className="content">
-                  <div className="text">NEW VISITORS</div>
+                  <div className="text">TOTAL COMPANY</div>
                   <div
                     className="number count-to"
                     data-from="0"
@@ -81,8 +100,188 @@ class HomeComponent extends React.Component {
                     data-speed="1000"
                     data-fresh-interval="20"
                   >
-                    1225
+                    {this.state.company_count}
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row clearfix">
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-pink hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">TOTAL EMPLOYEE</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="125"
+                    data-speed="15"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.employee_count}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-cyan hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">TOTAL PROFIT</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="257"
+                    data-speed="1000"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.profit_total}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-light-green hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">TOTAL SALES AMOUNT</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="243"
+                    data-speed="1000"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.sell_total}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-orange hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">MEDICINE EXPIRE IN WEEK</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="1225"
+                    data-speed="1000"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.medicine_expire_serializer_data}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row clearfix">
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-pink hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">COMPLETED REQUEST</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="125"
+                    data-speed="15"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.request_completed}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-cyan hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">PENDING REQUEST</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="257"
+                    data-speed="1000"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.request_pending}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-light-green hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">TODAY SALES AMOUNT</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="243"
+                    data-speed="1000"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.sell_amt_today}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box bg-orange hover-expand-effect">
+                <div className="icon">
+                  <i className="material-icons">bookmark</i>
+                </div>
+                <div className="content">
+                  <div className="text">TODAY SALES PROFIT</div>
+                  <div
+                    className="number count-to"
+                    data-from="0"
+                    data-to="1225"
+                    data-speed="1000"
+                    data-fresh-interval="20"
+                  >
+                    {this.state.profit_amt_today}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row clearfix">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div className="card">
+                <div className="header">
+                  <h2>Profit Chart</h2>
+                </div>
+                <div className="body">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row clearfix">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div className="card">
+                <div className="header">
+                  <h2>Sell Chart</h2>
+                </div>
+                <div className="body">
+                 
                 </div>
               </div>
             </div>
